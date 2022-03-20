091051d82b3826c5d00807e7f5a8cff3d4132fcb
XenoZohar <xeno.zohar@gmail.com>
2022-03-20 20:12:30 +0100

    Setting up interface and methods for CRUD

---
+++ NecklaceRepository/IPearlRepository.cs
@@ -0,0 +1,13 @@
+﻿using NecklaceModels;
+
+namespace NecklaceRepository
+{
+    public interface IPearlRepository
+    {
+        Task<Pearl> CreateAsync(Pearl pearl);
+        Task<IEnumerable<Pearl>> ReadAllAsync();
+        Task<Pearl> ReadAsync(int pearlId);
+        Task<Pearl> UpdateAsync(Pearl pearl);
+        Task<Pearl> DeleteAsync(int pearlId);
+    }
+}
